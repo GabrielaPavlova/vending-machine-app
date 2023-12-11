d7a99d80ca314440bcf6266da7faa5ba0bd2c947
Gabriela Pavlova <gabriela.pavlova6@abv.bg>
2023-12-11 10:48:37 +0200

    Create main components **Type:** Custom **Scope:** App module **Subject:** Add main components **Breaks:** N/A **Closes:** N/A **Details:**
    - Add main components only structural

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { CoinsComponent } from './coins/coins.component';
+import { BalanceComponent } from './balance/balance.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ProductComponent,
+    ProductListComponent,
+    CoinsComponent,
+    BalanceComponent,
   ],
   imports: [
     BrowserModule,
